503502f36635d1d7e510309ffe30588244c9fd85
Yaser <yaser@ualberta.ca>
2021-03-01 11:54:29 -0700

    - move mosques to own file - move mosqueMe script to script folder - use express-async-handler instead of try-catch blocks

---
+++ mosques.js
@@ -0,0 +1,49 @@
+const mosques = [
+    {
+        id: 1,
+        name: "Sahaba Mosque",
+        address: "9216 105 Ave NW, Edmonton, AB T5H 0J5",
+        imageUrl: "https://lirp-cdn.multiscreensite.com/bd8c603f/dms3rep/multi/opt/Dar-Al-Sunnah-Logo-584w.png",
+        websiteUrl: "https://www.daralsunnah.com/",
+        prayerTimesUrl: "https://mosque.me/prayer-times/daralsunnah/getdata.php",
+        script: "mosqueMe"
+    },
+    {
+        id: 2,
+        name: "Dar Al-Sunnah",
+        address: "9228 144 Ave NW, Edmonton, AB T5E 6A3",
+        imageUrl: "https://lirp-cdn.multiscreensite.com/7a517fc2/dms3rep/multi/opt/Screen+Shot+2019-01-24+at+10.12.23+PM-1920w.png",
+        websiteUrl: "https://www.sahabamosque.ca/",
+        prayerTimesUrl: "https://mosque.me/prayer-times/sahaba/getdata.php",
+        script: "mosqueMe"
+    },
+    {
+        id: 3,
+        name: "Al-Ameen Mosque",
+        address: "5410 122 Ave NW, Edmonton, AB T5W 1S2",
+        imageUrl: "https://lirp-cdn.multiscreensite.com/c53cbf10/dms3rep/multi/opt/Al-Ameen+Mosque_logo_211px206px_600px206px-960w.jpg",
+        websiteUrl: "https://www.alameenmosque.com/",
+        prayerTimesUrl: "https://mosque.me/prayer-times/alameen/getdata.php",
+        script: "mosqueMe"
+    },
+    {
+        id: 4,
+        name: "MCE Mosque",
+        address: "10721 86 Ave NW, Edmonton, AB T6E 2M8",
+        imageUrl: "https://lirp-cdn.multiscreensite.com/48f06d65/dms3rep/multi/opt/MCE-Logo-300x217-272w.png",
+        websiteUrl: "https://www.mcemosque.com/",
+        prayerTimesUrl: "http://mosque.me/prayer-times/mce/getdata.php",
+        script: "mosqueMe"
+    },
+    {
+        id: 5,
+        name: "Masjid Taqwa",
+        address: "10654 101 St NW, Edmonton, AB T5H 2S1",
+        imageUrl: "https://irp-cdn.multiscreensite.com/9f5dda22/3DvEOPzeSWuVT04cGQsG_At%20Taqwa%20Logo.png",
+        websiteUrl: "https://www.masjidtaqwa.ca/",
+        prayerTimesUrl: "http://mosque.me/prayer-times/taqwa/getdata.php",
+        script: "mosqueMe"
+    }
+]
+
+export default mosques
