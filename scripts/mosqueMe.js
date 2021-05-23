37e3718e21cd0a31055ffd3cf5353d557af2b1bf
Yaser <yaser@ualberta.ca>
2021-05-22 19:09:45 -0600

    Fix maghrib iqama typo + disable etag

--- scripts/mosqueMe.js
+++ scripts/mosqueMe.js
@@ -1,34 +1,33 @@
-import got from 'got'
+import got from "got";
 
 const run = async (url) => {
-    const mosqueMeRequest = await got(url);
-    const mosqueMeJson = JSON.parse(mosqueMeRequest.body)
-    const prayerTimes = 
-    {
-        fajr: {
-            athan: mosqueMeJson.fajr,
-            iqama: mosqueMeJson.fajri
-        },
-        dhuhr: {
-            athan: mosqueMeJson.thuhr,
-            iqama: mosqueMeJson.thuhri
-        },
-        asr: {
-            athan: mosqueMeJson.asr,
-            iqama: mosqueMeJson.asri
-        },
-        maghrib: {
-            athan: mosqueMeJson.maghrib,
-            iqama: mosqueMeJson.marghrib
-        },
-        isha: {
-            athan: mosqueMeJson.isha,
-            iqama: mosqueMeJson.ishai
-        }
-    }
-    return prayerTimes
-}
+  const mosqueMeRequest = await got(url);
+  const mosqueMeJson = JSON.parse(mosqueMeRequest.body);
+  const prayerTimes = {
+    fajr: {
+      athan: mosqueMeJson.fajr,
+      iqama: mosqueMeJson.fajri,
+    },
+    dhuhr: {
+      athan: mosqueMeJson.thuhr,
+      iqama: mosqueMeJson.thuhri,
+    },
+    asr: {
+      athan: mosqueMeJson.asr,
+      iqama: mosqueMeJson.asri,
+    },
+    maghrib: {
+      athan: mosqueMeJson.maghrib,
+      iqama: mosqueMeJson.maghribi,
+    },
+    isha: {
+      athan: mosqueMeJson.isha,
+      iqama: mosqueMeJson.ishai,
+    },
+  };
+  return prayerTimes;
+};
 
 export default {
-    run
-}
+  run,
+};
