22788b5c9e31efc57e4e3708a0eaa744287cd3a9
Yaser <yaser@ualberta.ca>
2021-07-11 15:44:52 -0600

    update url for mosque.me

--- mosques.js
+++ mosques.js
@@ -1,58 +1,63 @@
 const mosques = [
-    {
-        id: 1,
-        name: "Dar Al-Sunnah",
-        address: "9228 144 Ave NW, Edmonton, AB T5E 6A3",
-        imageUrl: "https://lirp-cdn.multiscreensite.com/bd8c603f/dms3rep/multi/opt/Dar-Al-Sunnah-Logo-584w.png",
-        websiteUrl: "https://www.daralsunnah.com/",
-        prayerTimesUrl: "https://mosque.me/prayer-times/daralsunnah/getdata.php",
-        scraperScript: "mosqueMe"
-    },
-    {
-        id: 2,
-        name: "Sahaba Mosque",
-        address: "9216 105 Ave NW, Edmonton, AB T5H 0J5",
-        imageUrl: "https://lirp-cdn.multiscreensite.com/7a517fc2/dms3rep/multi/opt/Screen+Shot+2019-01-24+at+10.12.23+PM-1920w.png",
-        websiteUrl: "https://www.sahabamosque.ca/",
-        prayerTimesUrl: "https://mosque.me/prayer-times/sahaba/getdata.php",
-        scraperScript: "mosqueMe"
-    },
-    {
-        id: 3,
-        name: "Al-Ameen Mosque",
-        address: "5410 122 Ave NW, Edmonton, AB T5W 1S2",
-        imageUrl: "https://lirp-cdn.multiscreensite.com/c53cbf10/dms3rep/multi/opt/Al-Ameen+Mosque_logo_211px206px_600px206px-960w.jpg",
-        websiteUrl: "https://www.alameenmosque.com/",
-        prayerTimesUrl: "https://mosque.me/prayer-times/alameen/getdata.php",
-        scraperScript: "mosqueMe"
-    },
-    {
-        id: 4,
-        name: "MCE Mosque",
-        address: "10721 86 Ave NW, Edmonton, AB T6E 2M8",
-        imageUrl: "https://lirp-cdn.multiscreensite.com/48f06d65/dms3rep/multi/opt/MCE-Logo-300x217-272w.png",
-        websiteUrl: "https://www.mcemosque.com/",
-        prayerTimesUrl: "http://mosque.me/prayer-times/mce/getdata.php",
-        scraperScript: "mosqueMe"
-    },
-    {
-        id: 5,
-        name: "Masjid Taqwa",
-        address: "10654 101 St NW, Edmonton, AB T5H 2S1",
-        imageUrl: "https://irp-cdn.multiscreensite.com/9f5dda22/3DvEOPzeSWuVT04cGQsG_At%20Taqwa%20Logo.png",
-        websiteUrl: "https://www.masjidtaqwa.ca/",
-        prayerTimesUrl: "http://mosque.me/prayer-times/taqwa/getdata.php",
-        scraperScript: "mosqueMe"
-    },
-    {
-        id: 6,
-        name: "Masjid Omari",
-        address: "6504 137 Ave NW, Edmonton, AB T5A 1R8",
-        imageUrl: "",
-        websiteUrl: "http://www.alomarimasjid.ca/",
-        prayerTimesUrl: "http://services.alomarimasjid.ca/prayertimes/alomari",
-        scraperScript:"omari"
-    }  
-]
+  {
+    id: 1,
+    name: "Dar Al-Sunnah",
+    address: "9228 144 Ave NW, Edmonton, AB T5E 6A3",
+    imageUrl:
+      "https://lirp-cdn.multiscreensite.com/bd8c603f/dms3rep/multi/opt/Dar-Al-Sunnah-Logo-584w.png",
+    websiteUrl: "https://www.daralsunnah.com/",
+    prayerTimesUrl: "https://prayertimes.mosque.me/daralsunnah/getdata.php",
+    scraperScript: "mosqueMe",
+  },
+  {
+    id: 2,
+    name: "Sahaba Mosque",
+    address: "9216 105 Ave NW, Edmonton, AB T5H 0J5",
+    imageUrl:
+      "https://lirp-cdn.multiscreensite.com/7a517fc2/dms3rep/multi/opt/Screen+Shot+2019-01-24+at+10.12.23+PM-1920w.png",
+    websiteUrl: "https://www.sahabamosque.ca/",
+    prayerTimesUrl: "https://prayertimes.mosque.me/sahaba/getdata.php",
+    scraperScript: "mosqueMe",
+  },
+  {
+    id: 3,
+    name: "Al-Ameen Mosque",
+    address: "5410 122 Ave NW, Edmonton, AB T5W 1S2",
+    imageUrl:
+      "https://lirp-cdn.multiscreensite.com/c53cbf10/dms3rep/multi/opt/Al-Ameen+Mosque_logo_211px206px_600px206px-960w.jpg",
+    websiteUrl: "https://www.alameenmosque.com/",
+    prayerTimesUrl: "https://prayertimes.mosque.me/alameen/getdata.php",
+    scraperScript: "mosqueMe",
+  },
+  {
+    id: 4,
+    name: "MCE Mosque",
+    address: "10721 86 Ave NW, Edmonton, AB T6E 2M8",
+    imageUrl:
+      "https://lirp-cdn.multiscreensite.com/48f06d65/dms3rep/multi/opt/MCE-Logo-300x217-272w.png",
+    websiteUrl: "https://www.mcemosque.com/",
+    prayerTimesUrl: "http://prayertimes.mosque.me/mce/getdata.php",
+    scraperScript: "mosqueMe",
+  },
+  {
+    id: 5,
+    name: "Masjid Taqwa",
+    address: "10654 101 St NW, Edmonton, AB T5H 2S1",
+    imageUrl:
+      "https://irp-cdn.multiscreensite.com/9f5dda22/3DvEOPzeSWuVT04cGQsG_At%20Taqwa%20Logo.png",
+    websiteUrl: "https://www.masjidtaqwa.ca/",
+    prayerTimesUrl: "http://prayertimes.mosque.me/taqwa/getdata.php",
+    scraperScript: "mosqueMe",
+  },
+  {
+    id: 6,
+    name: "Masjid Omari",
+    address: "6504 137 Ave NW, Edmonton, AB T5A 1R8",
+    imageUrl: "",
+    websiteUrl: "http://www.alomarimasjid.ca/",
+    prayerTimesUrl: "http://services.alomarimasjid.ca/prayertimes/alomari",
+    scraperScript: "omari",
+  },
+];
 
 export default mosques
